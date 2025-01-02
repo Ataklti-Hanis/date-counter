dbbc7df88d310fe641fd0fe462044770191ca3f8
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-02 08:38:51 +0300

    submit

--- src/App.js
+++ src/App.js
@@ -1,54 +1,5 @@
 import { useState } from "react";
 
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <Counter />
-//     </div>
-//   );
-// }
-
-// function Counter() {
-//   const [step, setStep] = useState(1);
-//   const [count, setCount] = useState(0);
-//   const date = new Date();
-//   date.setDate(date.getDate() + count);
-
-//   return (
-//     <div>
-//       <div>
-//         <input
-//           type="range"
-//           min="1"
-//           max="10"
-//           value={step}
-//           onChange={(e) => setStep(Number(e.target.value))}
-//         />
-//         <span>Step: {step}</span>
-//       </div>
-//       <div>
-//         <button onClick={() => setCount(count - step)}>-</button>
-//         <input
-//           type="text"
-//           value={count}
-//           onChange={(e) => setCount(Number(e.target.value))}
-//         />
-//         <button onClick={() => setCount(count + step)}>+</button>
-//       </div>
-//       <p>
-//         <span>
-//           {count === 0
-//             ? "today is "
-//             : count > 0
-//             ? `${count} days from today is`
-//             : `${count} days ago was `}
-//         </span>
-//         {date.toDateString()}{" "}
-//       </p>
-//     </div>
-//   );
-// }
-
 export default function App() {
   return (
     <div className="App">
@@ -58,8 +9,8 @@ export default function App() {
 }
 
 function Counter() {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(1);
 
   function handleClick() {
     setCount(0);
@@ -75,6 +26,7 @@ function Counter() {
           type="range"
           min="0"
           max="10"
+          value={step}
           onChange={(e) => setCount(setStep(Number(e.target.value)))}
         />
         <span>{step}</span>
